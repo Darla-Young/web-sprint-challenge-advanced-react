366d774668189ddaf4b0046003b810d274769c45
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-11-03 15:45:03 -0500

    fix get next index function

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -14,29 +14,33 @@ export default class AppClass extends React.Component {
   }
 
   getXY = () => {
-    let x = 0;
-    let y = 0;
+    let xy = {
+      0: [1,1],
+      1: [2,1],
+      2: [3,1],
+      3: [1,2],
+      4: [2,2],      
+      5: [3,2],      
+      6: [1,3],      
+      7: [2,3],      
+      8: [3,3]
+    };
     let index = this.state.index;
-    if (index - 3 > 2) {
-      x = 3;
-      y = index - 5;
-    } else if (index - 3 >= 0) {
-      x = 2;
-      y = index - 2;
-    } else {
-      x = 1;
-      y = index + 1;
-    }
+    console.log(index);
 
-    return ([x,y]);
+    return (xy[index]);
   }
 
   getXYMessage = () => {
     const coord = this.getXY();
     const x = coord[0];
     const y = coord[1];
-    
-    return `Coordinates (${x}, ${y})`;
+    this.setState({
+      ...this.state,
+      message: `Coordinates (${x}, ${y})`
+    });
+
+    return this.state;
   }
 
   reset = () => {
@@ -52,33 +56,34 @@ export default class AppClass extends React.Component {
   }
 
   getNextIndex = (direction) => {
-    let nextIndex = this.state.index;
+    let newIndex = this.state.index;
     if (direction === "up") {
-      if (nextIndex > 2) {
-        nextIndex -= 3;
-      }
-    } else if (direction === "right") {
-      if (!Number.isInteger((nextIndex + 1)/3)) {
-        nextIndex += 1;
-      }
-    } else if (direction === "down") {
-      if (nextIndex < 6) {
-        nextIndex += 3;
-      }
-    } else { // direction === "left"
-      if (!Number.isInteger(nextIndex/3)) {
-        nextIndex -= 1;
-      }
+      newIndex -= 3
+    } else if (direction === "right" && newIndex !== (2 || 5 || 8)) {
+      newIndex += 1
+    } else if (direction === "left" && newIndex !== (0 || 3 || 6)) {
+      newIndex -= 1
+    } else { // direction === "down"
+      newIndex += 3
+    }
+
+    if (8 >= newIndex && newIndex >= 0) {
+      this.setState({
+        ...this.state,
+        index: newIndex,
+        steps: this.state.steps + 1
+      })
     }
     
-    return nextIndex;
+    return this.state;
   }
 
   move = (id) => {
-    this.setState({
-      ...this.state, 
-      index: this.getNextIndex(id)
-    })
+    const oldIndex = this.state.index;
+    this.getNextIndex(id);
+    if(oldIndex !== this.state.index) {
+      this.getXYMessage()
+    }
   }
 
   onChange = (evt) => {
@@ -88,14 +93,7 @@ export default class AppClass extends React.Component {
       email: evt.target.value
     })}
     else if (id === 'reset') {this.reset()}
-    else {
-      this.move(id);
-      this.setState({
-        ...this.state, 
-        steps: this.state.steps + 1, 
-        coords: this.getXYMessage()
-      })
-    }
+    else {this.move(id)}
   }
 
   onSubmit = (evt) => {
@@ -114,7 +112,13 @@ export default class AppClass extends React.Component {
         email: ''
       })
     })
-    .catch(err => console.log(err));    
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        message: err.response.data.message,
+        email: ''
+      })
+    });    
   }
 
   render() {
@@ -152,3 +156,5 @@ export default class AppClass extends React.Component {
     )
   }
 }
+
+// Those numbers are calculated based on the data you're submitting. I notice that your coordinates don't update quite right and that it keeps adding to the move count even when it shouldn't be able to move. For example, it shouldn't be able to go higher than the top row and should be returning a message when you try and not increasing the move count in that case.
