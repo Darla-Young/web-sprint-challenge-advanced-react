5318b641ad7a12ebc8f3bfb2b5cf1baa775b9690
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-11-06 13:07:08 -0600

    class-based complete

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -5,15 +5,31 @@ export default class AppClass extends React.Component {
   constructor () {
     super ()
     this.state = {
-      coords: 'Coordinates (2, 2)',
-      email: '',
+      coords: 'Coordinates (2, 2)', // set by getXY()
       steps: 0,
+      index: 4, // the index the "B" is at
       message: '',
-      index: 4 // the index the "B" is at
+      email: ''
     }
   }
 
-  getXY = () => {
+  getNextIndex = (direction) => {
+    const oldIndex = this.state.index;
+    let newIndex = this.state.index;
+    if (direction === "up" && (oldIndex !== 0) && (oldIndex !== 1) && (oldIndex !== 2)) {
+      newIndex = newIndex - 3;
+    } else if (direction === "right" && (oldIndex !== 2) && (oldIndex !== 5) && (oldIndex !== 8)) {
+      newIndex = newIndex + 1;
+    } else if (direction === "left" && (oldIndex !== 0) && (oldIndex !== 3) && (oldIndex !== 6)) {
+      newIndex = newIndex - 1;
+    } else if (direction === "down" && (oldIndex !== 6) && (oldIndex !== 7) && (oldIndex !== 8)) { 
+      newIndex = newIndex + 3;
+    } else {newIndex}
+    
+    return newIndex;
+  }
+
+  getXY = (index) => {
     let xy = {
       0: [1,1],
       1: [2,1],
@@ -25,22 +41,12 @@ export default class AppClass extends React.Component {
       7: [2,3],      
       8: [3,3]
     };
-    let index = this.state.index;
-    console.log(index);
 
     return (xy[index]);
   }
 
-  getXYMessage = () => {
-    const coord = this.getXY();
-    const x = coord[0];
-    const y = coord[1];
-    this.setState({
-      ...this.state,
-      message: `Coordinates (${x}, ${y})`
-    });
-
-    return this.state;
+  getXYMessage = (coord) => {
+    return `Coordinates (${coord[0]}, ${coord[1]})`
   }
 
   reset = () => {
@@ -55,50 +61,35 @@ export default class AppClass extends React.Component {
     return this.state;
   }
 
-  getNextIndex = (direction) => {
-    let newIndex = this.state.index;
-    if (direction === "up") {
-      newIndex -= 3
-    } else if (direction === "right" && newIndex !== (2 || 5 || 8)) {
-      newIndex += 1
-    } else if (direction === "left" && newIndex !== (0 || 3 || 6)) {
-      newIndex -= 1
-    } else { // direction === "down"
-      newIndex += 3
-    }
-
-    if (8 >= newIndex && newIndex >= 0) {
-      this.setState({
-        ...this.state,
-        index: newIndex,
-        steps: this.state.steps + 1
-      })
-    }
-    
-    return this.state;
-  }
-
-  move = (id) => {
-    const oldIndex = this.state.index;
-    this.getNextIndex(id);
-    if(oldIndex !== this.state.index) {
-      this.getXYMessage()
-    }
-  }
-
   onChange = (evt) => {
-    let id = evt.target.id;
-    if (id === 'email') {this.setState({
+    this.setState({
       ...this.state, 
       email: evt.target.value
-    })}
-    else if (id === 'reset') {this.reset()}
-    else {this.move(id)}
+    })
+  }
+
+  onClick = (evt) => {
+    const id = evt.target.id;
+    if (id === "reset") {this.reset()}
+    else if (this.state.index !== this.getNextIndex(id)) {
+      console.log(this.state.index + " -> " + this.getNextIndex(id));
+      this.setState({
+        ...this.state,
+        index: this.getNextIndex(id),
+        steps: this.state.steps + 1,
+        coords: this.getXYMessage(this.getXY(this.getNextIndex(id)))
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        message: `You can't go ${id}`
+      })
+    }
   }
 
   onSubmit = (evt) => {
     evt.preventDefault();
-    const coords = this.getXY();
+    const coords = this.getXY(this.state.index);
     axios.post('http://localhost:9000/api/result', {
       x: coords[0], 
       y: coords[1], 
@@ -127,7 +118,7 @@ export default class AppClass extends React.Component {
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">{this.state.coords}</h3>
-          <h3 id="steps">You moved {this.state.steps} times</h3>
+          <h3 id="steps">You moved {this.state.steps} {this.state.steps === 1 ? "time" : "times"}</h3>
         </div>
         <div id="grid">
           {
@@ -142,11 +133,11 @@ export default class AppClass extends React.Component {
           <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left" onClick={this.onChange}>LEFT</button>
-          <button id="up" onClick={this.onChange}>UP</button>
-          <button id="right" onClick={this.onChange}>RIGHT</button>
-          <button id="down" onClick={this.onChange}>DOWN</button>
-          <button id="reset" onClick={this.onChange}>reset</button>
+          <button id="left" onClick={this.onClick}>LEFT</button>
+          <button id="up" onClick={this.onClick}>UP</button>
+          <button id="right" onClick={this.onClick}>RIGHT</button>
+          <button id="down" onClick={this.onClick}>DOWN</button>
+          <button id="reset" onClick={this.onClick}>reset</button>
         </div>
         <form onSubmit={this.onSubmit}>
           <input id="email" type="email" placeholder="type email" value={this.state.email} onChange={this.onChange} />
